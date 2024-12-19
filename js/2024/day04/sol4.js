631b0ddae497421e22aab18aa85c05542623df94
Sokolan <valerysokolan@gmail.com>
2024-12-19 16:44:40 +0200

    2024 day 4 part 2 solution

--- js/2024/day04/sol4.js
+++ js/2024/day04/sol4.js
@@ -11,12 +11,12 @@ function countWordsAt(matrix, word, location) {
   let count = 0;
   for (let horizontal = -1; horizontal <= 1; horizontal += 1) {
     for (let verical = -1; verical <= 1; verical += 1) {
-      for (let index = 0 ; index < word.length ; index += 1) {
-        const x = location.x + (horizontal * index);
-        const y = location.y + (verical * index);
+      for (let index = 0; index < word.length; index += 1) {
+        const x = location.x + horizontal * index;
+        const y = location.y + verical * index;
         if (!checkBoundaries(matrix, x, y)) break;
         if (word[index] !== matrix[x][y]) break;
-        if (index === (word.length - 1)) count += 1;
+        if (index === word.length - 1) count += 1;
       }
     }
   }
@@ -26,18 +26,53 @@ function countWordsAt(matrix, word, location) {
 function solvePartOne(inputPath) {
   const input = parseInput(inputPath);
   const inputMatrix = input.map((line) => line[0].split(""));
-  const res = inputMatrix.reduce((acc, currLine, x) => acc + currLine.reduce((acc, currCell, y) => 
-  acc + countWordsAt(inputMatrix, "XMAS", {x, y}), 0), 0);
-  
+  const res = inputMatrix.reduce(
+    (acc, currLine, x) =>
+      acc +
+      currLine.reduce(
+        (acc, currCell, y) => acc + countWordsAt(inputMatrix, "XMAS", { x, y }),
+        0
+      ),
+    0
+  );
+
   return String(res);
+}
+
+function checkXmas(matrix, location) {
+  const [x, y] = location;
+  let letters = "";
+  if (matrix[x][y] !== "A") return false;
+  for (let horizontal = -1; horizontal <= 1; horizontal += 2) {
+    for (let vertical = -1; vertical <= 1; vertical += 2) {
+      if (!checkBoundaries(matrix, x + horizontal, y + vertical)) return false;
+    }
+  }
+  const verticeA = matrix[x - 1][y - 1] + matrix[x + 1][y + 1];
+  const verticeB = matrix[x - 1][y + 1] + matrix[x + 1][y - 1];
+  if (verticeA !== "MS" && verticeA !== "SM") return false;
+  if (verticeB !== "MS" && verticeB !== "SM") return false;
+  return true;
 }
 
 function solvePartTwo(inputPath) {
   const input = parseInput(inputPath);
+  const inputMatrix = input.map((line) => line[0].split(""));
+  const res = inputMatrix.reduce(
+    (acc, currLine, x) =>
+      acc +
+      currLine.reduce(
+        (acc, currCell, y) => acc + checkXmas(inputMatrix, [x, y]),
+        0
+      ),
+    0
+  );
+  return String(res);
 }
 
 const inputPath = __dirname;
 
 const partOneExpected = "18";
-const partTwoExpected = "";
+const partTwoExpected = "9";
+
 runner(solvePartOne, partOneExpected, solvePartTwo, partTwoExpected, inputPath);
