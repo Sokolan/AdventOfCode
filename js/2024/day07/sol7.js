8085cd12da3ac9d7107ea4665b9d0970b9fd0ccd
Sokolan <valerysokolan@gmail.com>
2024-12-24 13:19:12 +0200

    day 7 part 2 solution

--- js/2024/day07/sol7.js
+++ js/2024/day07/sol7.js
@@ -11,42 +11,57 @@ function getSumsAndNumbers(input) {
   return [sums, numbersLists];
 }
 
-function isSumLegalRec(sum, accSum, index, numbersList) {
+function isSumLegalRec(sum, accSum, index, numbersList, operators) {
   if (index === numbersList.length) {
     return sum === accSum;
   }
-  return (
-    isSumLegalRec(sum, accSum + numbersList[index], index + 1, numbersList) ||
-    isSumLegalRec(sum, accSum * numbersList[index], index + 1, numbersList)
+  if (accSum > sum) return false;
+  return operators.some((operator) =>
+    isSumLegalRec(
+      sum,
+      operator(accSum, numbersList[index]),
+      index + 1,
+      numbersList,
+      operators
+    )
   );
 }
 
-function isSumLegal(sum, numbersList) {
-  return isSumLegalRec(sum, numbersList[0], 1, numbersList);
+function isSumLegal(sum, numbersList, operators) {
+  return isSumLegalRec(sum, numbersList[0], 1, numbersList, operators);
 }
 
-function countLegalSums(sums, numbersList) {
+function countLegalSums(sums, numbersList, operators) {
   return sums.reduce(
     (accSum, sum, index) =>
-      isSumLegal(sum, numbersList[index]) ? (accSum += sum) : accSum,
+      isSumLegal(sum, numbersList[index], operators) ? (accSum += sum) : accSum,
     0
   );
 }
 
 function solvePartOne(inputPath) {
   const input = parseInput(inputPath);
+  const operators = [(a, b) => a + b, (a, b) => a * b];
 
   const [sums, numbersLists] = getSumsAndNumbers(input);
-  return String(countLegalSums(sums, numbersLists));
+  return String(countLegalSums(sums, numbersLists, operators));
 }
 
 function solvePartTwo(inputPath) {
   const input = parseInput(inputPath);
+  const operators = [
+    (a, b) => a + b,
+    (a, b) => a * b,
+    (a, b) => Number.parseInt(String(a) + String(b)),
+  ];
+
+  const [sums, numbersLists] = getSumsAndNumbers(input);
+  return String(countLegalSums(sums, numbersLists, operators));
 }
 
 const inputPath = __dirname;
 
 const partOneExpected = "3749";
-const partTwoExpected = "";
+const partTwoExpected = "11387";
 
 runner(solvePartOne, partOneExpected, solvePartTwo, partTwoExpected, inputPath);
