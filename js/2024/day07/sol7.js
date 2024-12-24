07443e66e43db0e47978904a11cc497a6130f11a
Sokolan <valerysokolan@gmail.com>
2024-12-24 12:44:37 +0200

    day 7 part 1 solution

--- js/2024/day07/sol7.js
+++ js/2024/day07/sol7.js
@@ -1,8 +1,45 @@
 const runner = require("../../utils/runner");
 const parseInput = require("../../utils/parseInput");
+const { count } = require("console");
+
+function getSumsAndNumbers(input) {
+  const sums = [];
+  const numbersLists = [];
+  input.forEach((line) => {
+    sums.push(Number.parseInt(line[0].slice(0, -1)));
+    numbersLists.push(line.slice(1).map((number) => Number.parseInt(number)));
+  });
+  return [sums, numbersLists];
+}
+
+function isSumLegalRec(sum, accSum, index, numbersList) {
+  if (index === numbersList.length) {
+    return sum === accSum;
+  }
+  if (isSumLegalRec(sum, accSum + numbersList[index], index + 1, numbersList))
+    return true;
+  if (isSumLegalRec(sum, accSum * numbersList[index], index + 1, numbersList))
+    return true;
+  return false;
+}
+
+function isSumLegal(sum, numbersList) {
+  return isSumLegalRec(sum, numbersList[0], 1, numbersList);
+}
+
+function countLegalSums(sums, numbersList) {
+  let legalSums = 0;
+  sums.forEach((sum, index) => {
+    if (isSumLegal(sum, numbersList[index])) legalSums += sum;
+  });
+  return legalSums;
+}
 
 function solvePartOne(inputPath) {
   const input = parseInput(inputPath);
+
+  const [sums, numbersLists] = getSumsAndNumbers(input);
+  return String(countLegalSums(sums, numbersLists));
 }
 
 function solvePartTwo(inputPath) {
@@ -11,7 +48,7 @@ function solvePartTwo(inputPath) {
 
 const inputPath = __dirname;
 
-const partOneExpected = "";
+const partOneExpected = "3749";
 const partTwoExpected = "";
 
 runner(solvePartOne, partOneExpected, solvePartTwo, partTwoExpected, inputPath);
