9e470a0e38e4cb644b9ee7a7643cfde9c29bd547
Sokolan <valerysokolan@gmail.com>
2025-01-03 08:39:44 +0200

    day 11 part 2 solution

--- js/2024/day11/sol11.js
+++ js/2024/day11/sol11.js
@@ -8,39 +8,62 @@ function splitStone(stone) {
   ];
 }
 
+function addStonesToMap(stonesMap, stone, stonesToAdd) {
+  const currentStonesCount = stonesMap.get(stone);
+  if (!currentStonesCount) {
+    stonesMap.set(stone, stonesToAdd);
+  } else {
+    stonesMap.set(stone, currentStonesCount + stonesToAdd);
+  }
+}
+
+function countStonesInMap(stonesMap) {
+  let stonesCount = 0;
+  stonesMap.forEach(
+    (stoneCount) => (stonesCount += Number.parseInt(stoneCount))
+  );
+  return stonesCount;
+}
+
 function makeMoves(stones, numberOfMoves) {
-  let stonesArray = stones.slice();
+  let stonesMap = new Map();
+  stones.forEach((stone) => {
+    stonesMap.set(stone, 1);
+  });
   for (let i = 0; i < numberOfMoves; i += 1) {
-    const newStones = [];
-    stonesArray.forEach((stone, index) => {
+    const tmpMap = new Map();
+    stonesMap.forEach((stonesCount, stone) => {
       if (stone.localeCompare("0") === 0) {
-        stonesArray[index] = "1";
+        addStonesToMap(tmpMap, "1", stonesCount);
       } else if (stone.length % 2 === 0) {
         const [leftStone, rightStone] = splitStone(stone);
-        newStones.push(leftStone);
-        stonesArray[index] = rightStone;
+        addStonesToMap(tmpMap, leftStone, stonesCount);
+        addStonesToMap(tmpMap, rightStone, stonesCount);
       } else {
-        stonesArray[index] = String(Number.parseInt(stone) * 2024);
+        const stone2024 = String(Number.parseInt(stone) * 2024);
+        addStonesToMap(tmpMap, stone2024, stonesCount);
       }
     });
-    stonesArray = stonesArray.concat(newStones);
+    stonesMap = tmpMap;
   }
-  return stonesArray;
+  return countStonesInMap(stonesMap);
 }
 
 function solvePartOne(inputPath) {
   const input = parseInput(inputPath)[0];
-  const numberOfStones = makeMoves(input, 25).length;
+  const numberOfStones = makeMoves(input, 25);
   return String(numberOfStones);
 }
 
 function solvePartTwo(inputPath) {
-  const input = parseInput(inputPath);
+  const input = parseInput(inputPath)[0];
+  const numberOfStones = makeMoves(input, 75);
+  return String(numberOfStones);
 }
 
 const inputPath = __dirname;
 
 const partOneExpected = "55312";
-const partTwoExpected = "";
+const partTwoExpected = "65601038650482";
 
 runner(solvePartOne, partOneExpected, solvePartTwo, partTwoExpected, inputPath);
